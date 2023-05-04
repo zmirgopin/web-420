30767995aec2cda01661c227e9dbee62f544c19d
zmirgopin <zahavagopin@gmail.com>
2023-05-04 14:13:15 -0400

    week 8

--- routes/gopin-composer-routes.js
+++ routes/gopin-composer-routes.js
@@ -1,3 +1,9 @@
+/**
+	Title: gopin-composer-routes.js
+    Author: Zahava Gopin
+    Date: 4 May 2023
+    Description: MongoDB Shell routes for composer.
+ */
 const express = require('express');
 const router = express.Router();
 const Composer = require('../models/composer.js');
@@ -147,3 +153,125 @@ router.post('/api/composers', async(req, res) => {
 app.listen(PORT, () => {
     console.log('Application started and listening on PORT' + PORT);
 })
+
+/**
+ * updateComposerById
+ * @openapi
+ * /api/composers/:id:
+ *   put:
+ *     tags:
+ *       - Composer
+ *     name: updateComposerById
+ *     description: API for updating an existing document in MongoDB by ID.
+ *     summary: Updates a document in MongoDB. 
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Id to filter the collection by. 
+ *         schema: 
+ *           type: string
+ *     requestBody:
+ *       description: Composer information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - type
+ *             properties:
+ *               type:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: composer updated
+ *       '401':
+ *          description: Invalid composerId
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.put('api/composers/:id', async(req, res) => {
+    try{
+        Composer.findOne({'_id': req.params.id}, function(err, composer){
+            if (err) {
+                console.log(err);
+                res.status(501).send({
+                    'message': `MongoDB Exception: ${err}`
+                })
+            }else{
+                composer.set({
+                    id: req.body.id,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName
+                });
+
+                composer.save(function(err, updatedComposer){
+                    if (err){
+                        console.log(err);
+                        res.status(401).send({
+                            'message': `Invalid composerId`
+                        })
+                    }else{
+                        console.log(updatedComposer);
+                        res.json(updatedComposer);
+                    }
+                })
+            }
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            'message': `Server Exception: ${e.message}`
+        })
+    }
+})
+
+/**
+ * deleteComposerById
+ * @openapi
+ * /api/composers/:id:
+ *   delete:
+ *     tags:
+ *       - Composers
+ *     name: deleteComposerById
+ *     description: API for deleting a document from MongoDB by Id.
+ *     summary: Removes a document from MongoDB.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Id of the document to remove. 
+ *         schema: 
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Composer deleted
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+
+router.delete('api/composers/:id', async (req, res) => {
+    try {
+        Composer.findByIdAndDelete({'_id': req.params.id}, function(err, composer){
+            if (err) {
+                console.log(err);
+                res.status(501).send({
+                    'message': `MongoDB Exception: ${err}`
+                })
+            } else {
+                console.log(composer);
+                res.json(composer);
+            }
+        })
+    } catch (e){
+        console.log(e);
+        res.status(500).send({
+            'message': `Server Exception: ${e.message}`
+        })
+    }
+})
+
+module.exports = router
